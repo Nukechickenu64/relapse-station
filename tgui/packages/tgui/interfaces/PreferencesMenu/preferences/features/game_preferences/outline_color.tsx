caf08b7ec5a3ab1d5772a1e7b2e79c8706a0c08e
Bob <bobjogaiscoding@gmail.com>
2022-07-14 01:13:46 -0300

    decapitalize

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/outline_color.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/outline_color.tsx
@@ -1,7 +1,7 @@
 import { Feature, FeatureColorInputNoMutant } from "../base";
 
 export const outline_color: Feature<string> = {
-  name: "Outline Color",
+  name: "Outline color",
   category: "UI",
   description: "Color of outline effect of objects hidden by frills.",
   component: FeatureColorInputNoMutant,
